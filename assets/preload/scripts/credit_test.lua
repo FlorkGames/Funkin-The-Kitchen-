5ed23f9cde120e68ecfee2162e0f8d3d8d26199d
FlorkGames <105853494+FlorkGames@users.noreply.github.com>
2022-09-12 17:39:57 -0300

    Add files via upload

---
+++ assets/preload/scripts/credit_test.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('meet-the-mist', songName .. "messysalk", 0, 0, 0); --you can change the values as you want
+    setTextSize('meet-the-mist', 0);--change this for the stupid size
+    addLuaText('meet-the-mist');--all of this was made by Mr. fazbear and sansandre
+end
